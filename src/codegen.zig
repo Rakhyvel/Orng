55c3816780ebbe6038f28b3d1881868a6ce5e948
rakhyvel <josephmshimel@gmail.com>
2023-10-01 20:57:32 -0700

    add debug inc file

--- src/codegen.zig
+++ src/codegen.zig
@@ -30,10 +30,11 @@ pub fn generate(__program: *Program, file: *std.fs.File) !void {
         \\#include <stdint.h>
         \\#include <stdlib.h>
         \\
+        \\#include "debug.inc"
+        \\
         \\
     , .{});
 
-    try generateDebug(file);
     try generateFunctionTypedefs(&program.types, file);
     try generateInternedStrings(program.interned_strings, file);
     try file.writer().print("/* Function forward definitions */\n", .{});
@@ -222,37 +223,6 @@ fn generateFunctions(callGraph: *CFG, out: *std.fs.File) !void {
     for (callGraph.children.items) |child| {
         try generateFunctions(child, out);
     }
-}
-
-fn generateDebug(out: *std.fs.File) !void {
-    try out.writer().print("/* Debug information */\n", .{});
-    try out.writer().print(
-        \\static const char* $lines[1024];
-        \\static uint16_t $line_idx = 0;
-        \\
-        \\inline static void $panic(const char *restrict msg) {{
-        \\    fprintf(stderr, "panic: %s\n", msg);
-        \\    for(uint16_t $i = 0; $i < $line_idx; $i++) {{
-        \\        fprintf(stderr, "%s\n", $lines[$line_idx - $i - 1]);
-        \\    }}
-        \\    exit(1);
-        \\}}
-        \\
-        \\inline static void $bounds_check(const int64_t idx, const int64_t length, const char *restrict line) {{
-        \\    if (0 > idx || idx >= length) {{
-        \\        $lines[$line_idx++] = line;
-        \\        $panic("bounds check failed");
-        \\    }}
-        \\}}
-        \\
-        \\inline static void $tag_check(const int64_t tag, const int64_t sel, const char *restrict line) {{
-        \\    if (tag != sel) {{
-        \\        $lines[$line_idx++] = line;
-        \\        $panic("inactive field");
-        \\    }}
-        \\}}
-        \\
-    , .{});
 }
 
 fn generateMainFunction(callGraph: *CFG, out: *std.fs.File) !void {
--- tests/integration/build/functions/mut-param.c
+++ tests/integration/build/functions/mut-param.c
@@ -4,31 +4,8 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-/* Debug information */
-static const char* $lines[1024];
-static uint16_t $line_idx = 0;
+#include "debug.inc"
 
-inline static void $panic(const char *restrict msg) {
-    fprintf(stderr, "panic: %s\n", msg);
-    for(uint16_t $i = 0; $i < $line_idx; $i++) {
-        fprintf(stderr, "%s\n", $lines[$line_idx - $i - 1]);
-    }
-    exit(1);
-}
-
-inline static void $bounds_check(const int64_t idx, const int64_t length, const char *restrict line) {
-    if (0 > idx || idx >= length) {
-        $lines[$line_idx++] = line;
-        $panic("bounds check failed");
-    }
-}
-
-inline static void $tag_check(const int64_t tag, const int64_t sel, const char *restrict line) {
-    if (tag != sel) {
-        $lines[$line_idx++] = line;
-        $panic("inactive field");
-    }
-}
 /* Function forward definitions */
 int64_t _1_main();
 int64_t _3_sum_down(int64_t _3_x);
