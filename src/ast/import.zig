3d063a57d7dc204d141b23c019989a80cc85c426
Rakhyvel <josephmshimel@gmail.com>
2025-03-22 16:37:32 -0600

    simplify imports

--- src/ast/import.zig
+++ src/ast/import.zig
@@ -39,19 +39,12 @@ fn next_anon_name(class: []const u8, allocator: std.mem.Allocator) []const u8 {
     return (out.toOwned() catch unreachable).?;
 }
 
+/// Converts imports to constant declarations
 pub fn flat(self: Self, ast: *ast_.AST, asts: *std.ArrayList(*ast_.AST), idx: usize) walker_.Error!usize {
-    if (ast.* != .import) { // +1
-        return 0; // +1
+    if (ast.* != .import) {
+        return 0;
     }
 
-    // If the import is complex, split it up into a root import, and const decl's off of it
-    //   Remember: modules are _just_ named unit types!
-    //   So something like:
-    //     import a::b::c
-    //   would become:
-    //     import a as $anon_module_0
-    //     const $anon_module_1 = $anon_module_0::b
-    //     const c = $anon_module_1::c
     if (ast.import.pattern.* == .pattern_symbol and ast.import.pattern.pattern_symbol.kind == .import) {
         // Re-arrange to be a decl for the import
         const common = ast_.AST_Common{ ._token = ast.token(), ._type = null };
@@ -68,77 +61,104 @@ pub fn flat(self: Self, ast: *ast_.AST, asts: *std.ArrayList(*ast_.AST), idx: us
         _ = try self.resolve_import(ast.decl.pattern);
         return 0;
     } else if (ast.import.pattern.* == .access) {
-        // given lhms::a0::a1::rmhs
-        // ((lmhs::a0)::a1)::rmhs
-        // The lmhs should be turned into an anon import decl
-        // an.. are consts that are accesses of the previous, with anon names
-        // rmhs is a const access with the real name
-        var curr = ast.import.pattern;
-        var terms = std.ArrayList(*ast_.AST).init(self.compiler.allocator());
-        defer terms.deinit();
-
-        while (curr.* == .access) : (curr = curr.lhs()) {
-            terms.append(curr.rhs()) catch unreachable;
-        }
-        terms.append(curr) catch unreachable;
-
-        var anon_names = std.ArrayList([]const u8).init(self.compiler.allocator());
-        defer anon_names.deinit();
-
-        for (0.., terms.items) |i, term| {
-            anon_names.append(
-                if (i == 0) term.token().data else next_anon_name("anon", self.compiler.allocator()),
-            ) catch unreachable;
-        }
-
-        for (0.., terms.items) |i, term| {
-            if (i < terms.items.len - 1) {
-                // Insert `const rhs = lhs::rhs`
-                const init = ast_.AST.create_access(
-                    ast.token(),
-                    ast_.AST.create_identifier(Token.init_simple(anon_names.items[i + 1]), self.compiler.allocator()),
-                    terms.items[i],
-                    self.compiler.allocator(),
-                );
-                const const_decl = ast_.AST.create_decl(
-                    ast.import.pattern.token(),
-                    ast_.AST.create_pattern_symbol(
-                        ast.token(),
-                        .import_inner,
-                        anon_names.items[i],
-                        self.compiler.allocator(),
-                    ),
-                    ast_.AST.create_type_of(ast.token(), init, self.compiler.allocator()),
-                    init,
-                    false,
-                    self.compiler.allocator(),
-                );
-                asts.insert(idx, const_decl) catch unreachable;
-            } else {
-                const common = ast_.AST_Common{ ._token = ast.token(), ._type = null };
-                ast.* = ast_.AST{ .decl = .{
-                    .common = common,
-                    .symbols = std.ArrayList(*Symbol).init(self.compiler.allocator()),
-                    .pattern = ast_.AST.create_pattern_symbol(
-                        ast.token(),
-                        .{ .import = .{ .real_name = term.token().data } },
-                        anon_names.items[i],
-                        self.compiler.allocator(),
-                    ),
-                    .type = primitives_.type_type,
-                    .init = primitives_.unit_type,
-                    ._top_level = true,
-                    .is_alias = false,
-                    .prohibit_defaults = false,
-                } };
-                _ = try self.resolve_import(ast.decl.pattern);
-            }
-        }
-
-        return terms.items.len - 1;
+        return self.unwrap_access_imports(ast, asts, idx);
     }
 
     return 0;
+}
+
+/// Rewrites import access-sequences of the form:
+///   import aaa::bbb::ccc
+/// to:
+///   import aaa as anon0
+///   const anon1 = aaa::bbb
+///   const ccc = anon1::ccc
+fn unwrap_access_imports(self: Self, ast: *ast_.AST, asts: *std.ArrayList(*ast_.AST), idx: usize) !usize {
+    var terms = self.create_terms(ast);
+    defer terms.deinit();
+
+    var anon_names = self.create_anon_names(&terms);
+    defer anon_names.deinit();
+
+    for (0.., terms.items) |i, term| {
+        if (i < terms.items.len - 1) {
+            // Not the last term
+            // Insert `const rhs = lhs::rhs`
+            const init = ast_.AST.create_access(
+                ast.token(),
+                ast_.AST.create_identifier(Token.init_simple(anon_names.items[i + 1]), self.compiler.allocator()),
+                terms.items[i],
+                self.compiler.allocator(),
+            );
+            const const_decl = ast_.AST.create_decl(
+                ast.import.pattern.token(),
+                ast_.AST.create_pattern_symbol(
+                    ast.token(),
+                    .import_inner,
+                    anon_names.items[i],
+                    self.compiler.allocator(),
+                ),
+                ast_.AST.create_type_of(ast.token(), init, self.compiler.allocator()),
+                init,
+                false,
+                self.compiler.allocator(),
+            );
+            asts.insert(idx, const_decl) catch unreachable;
+        } else {
+            // The last term
+            const common = ast_.AST_Common{ ._token = ast.token(), ._type = null };
+            ast.* = ast_.AST{ .decl = .{
+                .common = common,
+                .symbols = std.ArrayList(*Symbol).init(self.compiler.allocator()),
+                .pattern = ast_.AST.create_pattern_symbol(
+                    ast.token(),
+                    .{ .import = .{ .real_name = term.token().data } },
+                    anon_names.items[i],
+                    self.compiler.allocator(),
+                ),
+                .type = primitives_.type_type,
+                .init = primitives_.unit_type,
+                ._top_level = true,
+                .is_alias = false,
+                .prohibit_defaults = false,
+            } };
+            _ = try self.resolve_import(ast.decl.pattern);
+        }
+    }
+
+    return terms.items.len - 1;
+}
+
+/// Creates a list of terms from an access-sequence in reverse order.
+/// Turns:
+///   aaa::bbb::ccc
+/// int:
+///   [ccc, bbb, aaa]
+fn create_terms(self: Self, ast: *ast_.AST) std.ArrayList(*ast_.AST) {
+    var curr = ast.import.pattern;
+    var terms = std.ArrayList(*ast_.AST).init(self.compiler.allocator());
+
+    while (curr.* == .access) : (curr = curr.lhs()) {
+        terms.append(curr.rhs()) catch unreachable;
+    }
+    terms.append(curr) catch unreachable;
+    return terms;
+}
+
+/// Creates a list of anonymous names to use when unwrapping an access-sequence import, where the first is the actual
+/// name, and every other is an anonymous name.
+/// Turns:
+///    aaa::bbb::ccc
+/// into:
+///    [ccc, anon0, anon1]
+fn create_anon_names(self: Self, terms: *std.ArrayList(*ast_.AST)) std.ArrayList([]const u8) {
+    var anon_names = std.ArrayList([]const u8).init(self.compiler.allocator());
+    for (0.., terms.items) |i, term| {
+        anon_names.append(
+            if (i == 0) term.token().data else next_anon_name("anon", self.compiler.allocator()),
+        ) catch unreachable;
+    }
+    return anon_names;
 }
 
 /// Given an import pattern symbol `ast`, resolve the module symbol that it refers to, potentially compiling it if
