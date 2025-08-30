38708c826df0a9c6329310bbcc62826d7bf2cd4b
Rakhyvel <josephmshimel@gmail.com>
2025-08-30 12:58:10 -0600

    simplify type-set.zig

--- src/ast/type-set.zig
+++ src/ast/type-set.zig
@@ -17,6 +17,7 @@ pub fn deinit(self: *Self) void {
     self.types.deinit();
 }
 
+/// Adds a type to the type set. Returns the dependency node for the corresponding type
 pub fn add(self: *Self, oldast_: *ast_.AST, allocator: std.mem.Allocator) ?*Dependency_Node {
     const ast = oldast_.expand_type(allocator);
     if (self.get(ast)) |dag| {
@@ -24,51 +25,53 @@ pub fn add(self: *Self, oldast_: *ast_.AST, allocator: std.mem.Allocator) ?*Depe
         return dag;
     }
     switch (ast.*) {
-        .function => {
-            var dag = Dependency_Node.init(ast, self.types.items.len, allocator);
-            self.types.append(dag) catch unreachable;
-            if (self.add(ast.lhs(), allocator)) |domain| {
-                dag.add_dependency(domain);
-            }
-            if (self.add(ast.rhs(), allocator)) |codomain| {
-                dag.add_dependency(codomain);
-            }
-            return dag;
-        },
-        .product, .sum_type, .untagged_sum_type => {
-            var dag = Dependency_Node.init(ast, self.types.items.len, allocator);
-            self.types.append(dag) catch unreachable;
-            for (ast.children().items) |term| {
-                if (self.add(term, allocator)) |dependency| {
-                    if (term.* != .addr_of)
-                        dag.add_dependency(dependency);
-                }
-            }
-            return dag;
-        },
-        .dyn_type => {
-            const dag = Dependency_Node.init(ast, self.types.items.len, allocator);
-            self.types.append(dag) catch unreachable;
-            return dag;
-        },
-        .annotation => {
-            return self.add(ast.annotation.type, allocator);
-        },
+        .function => return self.add_function(ast, allocator),
+        .product, .sum_type, .untagged_sum_type => return self.add_aggregate(ast, allocator),
+        .dyn_type => return self.add_dyn_type(ast, allocator),
+        .annotation => return self.add(ast.annotation.type, allocator),
         .addr_of => {
             _ = self.add(ast.expr(), allocator);
             return null;
         },
-        .identifier, .unit_type, .anyptr_type => {
-            // Do not add to Dependency_Node
-            return null;
-        },
-        else => {
-            std.debug.panic("unknown: {}", .{ast});
-            return null;
-        },
+        .identifier, .unit_type, .anyptr_type => return null, // Do not add to Dependency_Node
+        else => std.debug.panic("unknown: {}", .{ast}),
     }
 }
 
+/// Adds a function type to the type set
+fn add_function(self: *Self, function_type_ast: *ast_.AST, allocator: std.mem.Allocator) ?*Dependency_Node {
+    var dag = Dependency_Node.init(function_type_ast, self.types.items.len, allocator);
+    self.types.append(dag) catch unreachable;
+    if (self.add(function_type_ast.lhs(), allocator)) |domain| {
+        dag.add_dependency(domain);
+    }
+    if (self.add(function_type_ast.rhs(), allocator)) |codomain| {
+        dag.add_dependency(codomain);
+    }
+    return dag;
+}
+
+/// Adds an aggregate type (product, sum) to the type set
+fn add_aggregate(self: *Self, aggregate_ast: *ast_.AST, allocator: std.mem.Allocator) ?*Dependency_Node {
+    var dag = Dependency_Node.init(aggregate_ast, self.types.items.len, allocator);
+    self.types.append(dag) catch unreachable;
+    for (aggregate_ast.children().items) |term| {
+        if (self.add(term, allocator)) |dependency| {
+            if (term.* != .addr_of)
+                dag.add_dependency(dependency);
+        }
+    }
+    return dag;
+}
+
+/// Adds a dyn type to the type set
+fn add_dyn_type(self: *Self, dyn_type_ast: *ast_.AST, allocator: std.mem.Allocator) ?*Dependency_Node {
+    const dag = Dependency_Node.init(dyn_type_ast, self.types.items.len, allocator);
+    self.types.append(dag) catch unreachable;
+    return dag;
+}
+
+/// Retrieves the dependency node from the type set given a type
 pub fn get(self: *Self, oldast_: *ast_.AST) ?*Dependency_Node {
     const ast = oldast_;
     for (self.types.items) |dag| {
@@ -79,6 +82,7 @@ pub fn get(self: *Self, oldast_: *ast_.AST) ?*Dependency_Node {
     return null;
 }
 
+/// Debug function to print out the type set
 pub fn print(self: *Self) void {
     for (self.types.items) |dag| {
         dag.print();
